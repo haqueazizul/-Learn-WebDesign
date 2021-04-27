919d87ef88bad06873bcd4d2f2da736587171104
Azizul Haque <79374844+haqueazizul@users.noreply.github.com>
2021-04-27 22:44:34 +0600

    day7
    d6ne

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,6 +2,27 @@
 
 $(document) .ready(function(){
 
+	//STICKY MENUE
+	$(".js--services-section").waypoint(function(direction){
+		if (direction == "down") {
+			$("nav").addclass("sticky");
+
+		}
+		else{
+			$("nav").removeclass("sticky");
+
+
+		}
+
+
+	});
+		
+
+
+
+
+
+//MIXITUP
 var mixer = mixitup('.container');
 
 });
